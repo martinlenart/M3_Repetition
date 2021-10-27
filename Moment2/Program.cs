bf61f69cb3649c83f70775f6b18a8657d1102ceb
Martin Lenart <martin_lenart@icloud.com>
2021-10-27 11:16:13 +0200

    Final Update

---
+++ Moment2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Moment2
+{
+    public enum DogBreeds { GoldenRetriever, Schitzu, Labrador, Rottweiler}
+    class Dog
+    {
+        public DogBreeds Breed { get; init; }
+        public string Name { get; set; }
+        public DateTime BirthDay{ get; init; }
+
+        public int YearsOfAge
+        {
+            get
+            {
+                var age = DateTime.Now - BirthDay;
+                return age.Days/365;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is a {Breed}. He is {YearsOfAge} years old";
+        }
+
+    }
+    class Program
+    {
+        static int MaxNrOfDogs = 4;
+        static void Main(string[] args)
+        {
+            Dog[] Kennel = new Dog[MaxNrOfDogs];
+
+            Kennel[0] = new Dog { Breed = DogBreeds.GoldenRetriever, Name = "Luna", BirthDay = DateTime.Parse("2020-01-15")  };
+            Kennel[1] = new Dog { Breed = DogBreeds.Schitzu, Name = "Ralf", BirthDay = DateTime.Parse("2020-04-25") };
+            Kennel[2] = new Dog { Breed = DogBreeds.Labrador, Name = "Pontus", BirthDay = DateTime.Parse("2005-01-15") };
+            Kennel[3] = new Dog { Breed = DogBreeds.Rottweiler, Name = "Ralf", BirthDay = DateTime.Parse("2010-01-15") };
+            for (int i = 0; i < Kennel.Length; i++)
+            {
+                Console.WriteLine(Kennel[i]);
+            }
+
+            //Change Ralf to Marcus
+            string FindName = "Ralf";
+            string NewName = "Marcus";
+            for (int j = 0; j < Kennel.Length; j++)
+            {
+                if (Kennel[j].Name == FindName && Kennel[j].Breed == DogBreeds.Schitzu ||
+                    Kennel[j].Name == FindName && Kennel[j].Breed == DogBreeds.Rottweiler)
+                {
+                    Kennel[j].Name = NewName;
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < Kennel.Length; i++)
+            {
+                Console.WriteLine(Kennel[i]);
+            }
+
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Properties
+{
+    public class AClass<T> where T:class
+    {
+        public T field1;
+        public readonly T field2 = default;
+
+        public T prop1 { get; set; }
+        public T prop2 { get; init; }
+        public T prop3 { get; }
+        
+        public T[] array1;
+        public readonly T[] array2;
+
+        public AClass ()
+        {
+            field1 = default;
+            field2 = default;
+
+            prop1 = default;
+            prop2 = default;
+            prop3 = default;
+
+            array1 = new T[2];
+            array1[0] = default;
+            array1[1] = default;
+
+            array2 = new T[2];
+            array2[0] = default;
+            array2[1] = default;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AClass<int> myInstance1 = new AClass<int>();
+
+            myInstance1.field1 = 5;
+ //           myInstance.field2 = 5;
+
+            myInstance1.prop1 = 5;
+//            myInstance.prop2 = 5;
+//            myInstance.prop3 = 5;
+
+            myInstance1.array1[0] = 5;
+            myInstance1.array1[1] = 5;
+            myInstance1.array1 = default;
+
+            myInstance1.array2[0] = 5;
+            myInstance1.array2[1] = 5;
+            //            myInstance.array2 = new int[5];
+
+            AClass<string> myInstance2 = new AClass<string>();
+            myInstance2.field1 = "Hello";
+            myInstance2.array1[0] = "Hello";
+        }
+    }
+}
